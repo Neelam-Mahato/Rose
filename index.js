ede5e0d4546aad8366c1a4409ee748e744712871
Neelam-Mahato <141303237+Neelam-Mahato@users.noreply.github.com>
2024-02-07 19:04:13 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const indexRouter = require("./routes/index");
+require('dotenv').config();
 
 const app = express();
 app.use(express.static(path.join(__dirname, "public")));
