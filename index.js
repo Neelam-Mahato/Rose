ba119501c97ec7d98309d733eeeac55a3a42a83f
Neelam-Mahato <141303237+Neelam-Mahato@users.noreply.github.com>
2024-02-07 14:02:02 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const path = require("path");
+const indexRouter = require("./routes/index");
+
+const app = express();
+app.use(express.static(path.join(__dirname, "public")));
+
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+app.use("/", indexRouter);
+
+const PORT = process.env.PORT || 3143;
+app.listen(
+  PORT,
+  console.log(
+    `Server running LOVE on ${PORT} at ${new Date()}`
+  )
+);
